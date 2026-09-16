e082ad1d65a8dc7a3acd59aaead58b54b460edfe
Juanes_URBA <024juanurrego@gmail.com>
2026-09-16 12:31:14 -0500

    feat(frontend): implement product catalog and detail views

---
+++ src/app/core/services/product.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import { ProductResponse } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = `${environment.apiUrl}/products`;
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<ProductResponse[]> {
+    return this.http.get<ProductResponse[]>(this.apiUrl);
+  }
+
+  getProductById(id: number): Observable<ProductResponse> {
+    return this.http.get<ProductResponse>(`${this.apiUrl}/${id}`);
+  }
+
+  searchByName(name: string): Observable<ProductResponse[]> {
+    return this.http.get<ProductResponse[]>(`${this.apiUrl}/search`, {
+      params: { name }
+    });
+  }
+
+  getByCategory(category: string): Observable<ProductResponse[]> {
+    return this.http.get<ProductResponse[]>(`${this.apiUrl}/category/${category}`);
+  }
+}
